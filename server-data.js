aa1fc15bf64566be3a1a2ff0f9f3a7ac3290e446
arcooty <51170925+arcooty@users.noreply.github.com>
2026-09-14 06:16:29 +0100

    Replace technical homepage messaging

--- server-data.js
+++ server-data.js
@@ -1,7 +1,7 @@
 window.ARCADECRAFT = Object.freeze({
   address: 'oyna.robsarcade.online', version: '26.2', discord: 'https://discord.gg/GerdDHzMWp',
   services: [
-    {id:'velocity',name:'Ağ Geçidi',state:'online'}, {id:'lobby',name:'Lobi',state:'online'},
+    {id:'velocity',name:'Ağ Girişi',state:'online'}, {id:'lobby',name:'Lobi',state:'online'},
     {id:'survival',name:'Survival',state:'online'}, {id:'events',name:'Etkinlik',state:'scheduled'}
   ]
 });
